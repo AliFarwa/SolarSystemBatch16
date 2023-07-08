e5d1ba9e4ac8a041d200c5359ae184b96b2b1d29
AliFarwa <farwaali3013@gmail.com>
2023-07-08 12:33:30 -0400

    The basic solar system is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        solarSystemBasics Omnax= new solarSystemBasics();
+        Omnax.Moon="2";
+        Omnax.Sun="1";
+        Omnax.Planets="2";
+        Omnax.star="4";
+    }
+}
---
+++ src/solarSystemBasics.java
@@ -0,0 +1,11 @@
+public class solarSystemBasics {
+
+    String Planets;
+    String Moon;
+    String Sun;
+    String star;
+
+
+
+
+}
